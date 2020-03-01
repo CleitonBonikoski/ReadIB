0f063c052a3ee716ae4814ee762705854a0e3a20
cleiton bonikoski <cleitonbonikoski57@hotmail.com>
2020-03-01 18:53:17 -0300

    Criando estrutura de leitura do arquivo.ib

--- ReadIB/ReadIB/Program.cs
+++ ReadIB/ReadIB/Program.cs
@@ -1,8 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
+using System.Text.RegularExpressions;
+
 
 namespace ReadIB
 {
@@ -10,6 +10,38 @@ namespace ReadIB
     {
         static void Main(string[] args)
         {
+            
+            // Variável para medir tempo de execução.
+            var watch = new Stopwatch();
+            string[] lstPalavras;
+            //Iníco cálculo tempo de execução.
+            watch.Start();
+
+            string data = File.ReadAllText(@"C:\\SEG\\dados\\DADOS3.IB").ToString();
+
+            watch.Stop();
+
+            Console.WriteLine($"Tempo Execução em segundos: {watch.Elapsed.TotalSeconds.ToString()} ");
+
+            // Variável que irá validar os caracteres permitidos.
+            Regex rgx = new Regex(@"^[a-zA-Z]");           
+
+            // Irá ler a palavra ou frase digitada e separar por espaços.
+            lstPalavras = data.Split();
+            Console.Clear();
+
+            foreach (var palavra in lstPalavras)
+            {
+                if (rgx.IsMatch(palavra))
+                {
+                    Console.WriteLine(palavra);
+                    Console.ReadKey();
+                }
+
+            }
+
+            Console.ReadKey();
         }
+
     }
 }
